232cc4da36d8c393ba68da66e6996fb384c9eea6
riyosan <64902143+riyosan@users.noreply.github.com>
2021-12-04 06:09:24 +0700

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -8,21 +8,51 @@ def upload_dataset():
 	with st.sidebar.header("1. Upload your CSV data"):
 		data=st.file_uploader("Upload your input CSV file",type=['csv'])
 		return data
-def prepro(df):
-	#parsing
-	df.first_open=[parser.parse(i) for i in df.first_open]
-	df.enrolled_date=[parser.parse(i) if isinstance(i, str)else i for i in df.enrolled_date]
-#mengurangi enrolled_date dengan firs_open
-	df['selisih']=(df.enrolled_date-df.first_open).astype('timedelta64[h]')
-	df.loc[df.selisih>24, 'enrolled'] = 0
-#import top_screen
-	top_screens=pd.read_csv('/content/skripsi/top_screens.csv')
+def prepro(df1):
+	#menghitung ulang isi dari kolom screen_litst karena tidak pas di kolom num screens
+	df1['screen_list'] = df1.screen_list.astype(str) + ','
+	df1['num_screens'] = df1.screen_list.str.count(',')
+	#menghapus kolom numsreens yng lama
+	df1.drop(columns=['numscreens'], inplace=True)
+	container = st.columns((1.9, 1.1))
+	df1_types = df1.dtypes.astype(str)
+	with container[0]:
+		st.write(df1)
+    with container[1]:
+		st.write(df1_types)
+    #feature engineering
+    #karena kolom hour ada spasinya, maka kita ambil huruf ke 1 sampai ke 3
+	df1.hour=df1.hour.str.slice(1,3).astype(int)
+	#karena tipe data first_open dan enrolled_date itu adalah string, maka perlu diubah ke datetime
+	df1.first_open=[parser.parse(i) for i in df1.first_open]
+	#didalam dataset orang yg belum langganan itu NAN, maka jika i=string biarin, klo ga string diuah ke datetime kolom nan nya biarin tetap nat
+	df1.enrolled_date=[parser.parse(i) if isinstance(i, str)else i for i in df1.enrolled_date]
+	#membuat kolom selisih , yaitu menghitung berapa lama orang yg firs_open menjadi enrolled
+	df1['selisih']=(df1.enrolled_date-df1.first_open).astype('timedelta64[h]')
+	#karna digrafik menunjukkan orang kebanyakan enroll selama 24 jam pertama, maka kalau lebih dari 24 jam dianggap ga penting
+	df1.loc[df1.selisih>24, 'enrolled'] = 0
+	container2 = st.columns((1.9, 1.1))
+	df1_types = df1.dtypes.astype(str)
+	with container2[0]:
+		st.write(df1)
+    with container2[1]:
+		st.write(df1_types)
+    # mengambil dataset top screen
+	top_screens=pd.read_csv('/content/drive/MyDrive/kuliah/dataset/top_screens.csv')
+	# diubah ke numppy arry dan mengambil kolom ke2 saja karna kolom1 isinya nomor
 	top_screens=np.array(top_screens.loc[:,'top_screens'])
+	df = df1.copy()
+	#mengubah isi dari file top screen menjadi numerik
 	for i in top_screens:
 		df[i]=df.screen_list.str.contains(i).astype(int)
+	#semua item yang ada di file top screen dihilangkan dari kolom screen list
 	for i in top_screens:
 		df['screen_list']=df.screen_list.str.replace(i+',','')
-#menghapus double layar
+	#menghitung jumlah item non top screen yang(tersisa) ada di screenlist
+	df['lainnya']=df.screen_list.str.count(',')
+	st.write(df)
+	#menggabungkan item yang mirip mirip, seperti kredit 1 kredit 2 dan kredit 3
+	#funneling = menggabungkan beberapa screen yang sama dan menghapus layar yang sama
 	layar_loan = ['Loan','Loan2','Loan3','Loan4']
 	df['jumlah_loan']=df[layar_loan].sum(axis=1)
 	df.drop(columns=layar_loan, inplace=True)
@@ -38,36 +68,78 @@ def prepro(df):
 	layar_cc = ['CC1','CC1Category','CC3']
 	df['jumlah_cc']=df[layar_cc].sum(axis=1)
 	df.drop(columns=layar_cc, inplace=True)
-	df['lainnya']=df.screen_list.str.count(',')
-	st.write(df.head(10))
-#menghitung jumlah screen
-	df['screen_list'] = df.screen_list.astype(str) + ','
-	df['num_screens'] = df.screen_list.str.count(',')
-	df.drop(columns=['numscreens'], inplace=True)
-#mengubah kolom hour
-	df.hour=df.hour.str.slice(1,3).astype(int)
-
-#mendefenisikan variabel numerik
+	#menghilangkan kolom yang ga relevan
 	df_numerik=df.drop(columns=['user','first_open','screen_list','enrolled_date','selisih'], inplace=False)
 	df_types = df.dtypes.astype(str)
-	st.write(df_numerik.head(10))
-	df_types = df.dtypes.astype(str)
-	st.write(df_types)
-#membuat korelasi matrik
+	st.write(df_numerik)
+	#membuat plot korelasi tiap kolom dengan enrolled
 	korelasi = df_numerik.drop(columns=['enrolled'], inplace=False).corrwith(df_numerik.enrolled)
 	plot=korelasi.plot.bar(title='korelasi variabel')
 	st.set_option('deprecation.showPyplotGlobalUse', False)
 	st.pyplot()
-	df_numerik.to_csv('/content/skripsi/data', index=False)
+
+# 	#parsing
+# 	df1.first_open=[parser.parse(i) for i in df1.first_open]
+# 	df1.enrolled_date=[parser.parse(i) if isinstance(i, str)else i for i in df1.enrolled_date]
+# #mengurangi enrolled_date dengan firs_open
+# 	df1['selisih']=(df1.enrolled_date-df1.first_open).astype('timedelta64[h]')
+# 	df1.loc[df1.selisih>24, 'enrolled'] = 0
+# #import top_screen
+# 	top_screens=pd.read_csv('/content/skripsi/top_screens.csv')
+# 	top_screens=np.array(top_screens.loc[:,'top_screens'])
+# 	for i in top_screens:
+# 		df1[i]=df1.screen_list.str.contains(i).astype(int)
+# 	for i in top_screens:
+# 		df1['screen_list']=df1.screen_list.str.replace(i+',','')
+# #menghapus double layar
+# 	layar_loan = ['Loan','Loan2','Loan3','Loan4']
+# 	df1['jumlah_loan']=df1[layar_loan].sum(axis=1)
+# 	df1.drop(columns=layar_loan, inplace=True)
+
+# 	layar_saving = ['Saving1','Saving2','Saving2Amount','Saving4','Saving5','Saving6','Saving7','Saving8','Saving9','Saving10']
+# 	df1['jumlah_loan']=df1[layar_saving].sum(axis=1)
+# 	df1.drop(columns=layar_saving, inplace=True)
+
+# 	layar_credit = ['Credit1','Credit2','Credit3','Credit3Container','Credit3Dashboard']
+# 	df1['jumlah_credit']=df1[layar_credit].sum(axis=1)
+# 	df1.drop(columns=layar_credit, inplace=True)
+
+# 	layar_cc = ['CC1','CC1Category','CC3']
+# 	df1['jumlah_cc']=df1[layar_cc].sum(axis=1)
+# 	df1.drop(columns=layar_cc, inplace=True)
+# 	df1['lainnya']=df1.screen_list.str.count(',')
+# 	st.write(df1.head(10))
+# #menghitung jumlah screen
+# 	df1['screen_list'] = df1.screen_list.astype(str) + ','
+# 	df1['num_screens'] = df1.screen_list.str.count(',')
+# 	df1.drop(columns=['numscreens'], inplace=True)
+# #mengubah kolom hour
+# 	df1.hour=df1.hour.str.slice(1,3).astype(int)
+
+# #mendefenisikan variabel numerik
+# 	df1_numerik=df1.drop(columns=['first_open','screen_list','enrolled_date','selisih'], inplace=False)
+# 	df1_copy = df1_numerik.copy()
+# 	df1_numerik=df1.drop(columns=['user'], inplace=False)
+# 	df1_types = df1.dtypes.astype(str)
+# 	st.write(df1_numerik.head(10))
+# 	df1_types = df1.dtypes.astype(str)
+# 	st.write(df1_types)
+# #membuat korelasi matrik
+# 	korelasi = df1_numerik.drop(columns=['enrolled'], inplace=False).corrwith(df1_numerik.enrolled)
+# 	plot=korelasi.plot.bar(title='korelasi variabel')
+# 	st.set_option('deprecation.showPyplotGlobalUse', False)
+# 	st.pyplot()
+	df_numerik.to_csv('/content/skripsi/data/main_data.csv', index=False)
+	df1.to_csv('/content/skripsi/data/df1.csv', index=False)
 def app():
 	global data
 	filenya=upload_dataset()
 	if filenya is not None:
-		df=pd.read_csv(filenya)
+		df1=pd.read_csv(filenya)
 		if st.button("Load Data"):
-			st.write(df)
-			prepro(df)
+			st.write(df1)
+			prepro(df1)
 	else:
 		if st.button('Press to use Example Dataset'):
-			df = pd.read_csv('/content/skripsi/fintech_data.csv')
-			prepro(df)
+			df1 = pd.read_csv('/content/skripsi/fintech_data.csv')
+			prepro(df1)
